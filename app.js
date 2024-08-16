65829554a80ffd168dd88b056a84a6b64e207fa5
alibyt21 <alibyt21@gmail.com>
2024-08-16 19:21:55 +0330

    working version

---
+++ app.js
@@ -0,0 +1,56 @@
+const puppeteerCore = require("puppeteer-core");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.get("/pdfgn/download", async (req, res) => {
+    try {
+        // Execute the function
+        // Launch Puppeteer Core with the specified executable path and headless mode set to false
+        const browser = await puppeteerCore.connect({
+            browserWSEndpoint:
+                "wss://cranky-stonebraker-yf01bsczof.liara.run?token=f8SQ43ERLf6f8KuNRcuw",
+        });
+
+        // Open a new page
+        const page = await browser.newPage();
+
+        // Set viewport size to 1920px width and specify a height (e.g., 1080px)
+        await page.setViewport({ width: 1540, height: 1080 });
+
+        // Emulate media type to 'screen'
+        await page.emulateMediaType("screen");
+
+        // Navigate to the specified URL
+        await page.goto(
+            "https://typesanj.ir/rfheflxuriqpwpdf/?key=c51ce410c124a10e0db5e4b97fc2af39",
+            { waitUntil: "networkidle0", timeout: 200000 } // 200 seconds timeout
+        );
+
+        // Optionally, add custom styles or perform other manipulations to fit content within A4
+        // await page.addStyleTag({ content: 'body { transform: scale(0.75); transform-origin: top left; }' });
+
+        // Generate PDF from the page content in landscape mode with adjusted margins
+        const pdfBuffer = await page.pdf({
+            format: "A4",
+            printBackground: true,
+            margin: { top: "10mm", bottom: "10mm" },
+            landscape: false, // Consider using landscape mode if content is wider than it is tall
+        });
+
+        // Close the browser
+        await browser.close();
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader("Content-Disposition", "attachment; filename=generated.pdf");
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
